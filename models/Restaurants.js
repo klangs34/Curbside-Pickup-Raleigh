81105b64c3e3682efe82fbf56fa291e329d0aadb
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-11 10:14:36 -0400

    seeds updated

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -4,47 +4,48 @@ const Schema = mongoose.Schema;
 const RestaurantSchema = new Schema({
   name: {
     type: String,
-    unique: true
+    unique: true,
   },
   address: [
     {
       street: {
-        type: String
+        type: String,
       },
       city: {
-        type: String
+        type: String,
       },
       zip: {
         //need this?
-        type: Number
+        type: Number,
       },
       phone: {
-        type: String
-      }
-    }
+        type: String,
+      },
+    },
   ],
   hours: [
     {
+      //default to daily hours, add option in window that says select aditional days and will populate in an empty string to modify for Sat, Sun, etc
       days: {
-        type: String
+        type: String,
         //option to put in a checkbox? like M-F, S, S, etc?
       },
       open: {
-        type: Number
+        type: Number,
       },
       close: {
-        type: Number
-      }
-    }
+        type: Number,
+      },
+    },
   ],
   menu: [
     {
       web_url: { type: String },
       instagram: { type: String },
-      facebook: { type: String }
-    }
+      facebook: { type: String },
+    },
   ],
-  category: { type: String }
+  category: { type: String },
   //how to make a choice: food, alcohol, food and alcohol
 });
 
