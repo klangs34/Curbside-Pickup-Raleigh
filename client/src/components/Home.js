242abaf832f66a5e3565c0c4d56bea22a65040b6
kelz <kelsie.langston@yahoo.com>
2020-04-18 12:05:29 -0400

    keep current changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,48 +27,37 @@ class Home extends React.Component {
   handleFormSubmit = (event) => {
     event.preventDefault();
     // this.searchGiphy(this.state.search);
-    API.findLocation(this.state.search)
-      .then((data) => {
-        this.setState({
-          lat: data.data.results[0].geometry.location.lat,
-          lng: data.data.results[0].geometry.location.lng,
-        });
-        API.getRestaurants()
-          .then((response) => {
-            this.filterRest(response.data);
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
+    API.findLocation(this.state.search).then((data) => {
+      this.setState({
+        lat: data.data.results[0].geometry.location.lat,
+        lng: data.data.results[0].geometry.location.lng,
+      });
+      API.getRestaurants().then((response) => {
+        this.filterRest(response.data);
+      }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
   };
 
   componentDidMount = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.setState({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-          API.getRestaurants()
-            .then((data) => {
-              console.log(data);
-              this.filterRest(data.data);
-            })
-            .catch((err) => console.log(err));
-        },
-        () => {
-          API.getRestaurants()
-            .then((data) => {
-              this.filterRest(data.data);
-            })
-            .catch((err) => console.log(err));
-        }
-      );
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        API.getRestaurants().then((data) => {
+          console.log(data);
+          this.filterRest(data.data)
+        }).catch(err => console.log(err));
+      }, () => {
+        API.getRestaurants().then(data => {
+          this.filterRest(data.data);
+        }).catch(err => console.log(err));
+      });
     }
   };
 
-  filterRest = (restaurants) => {
+  filterRest = restaurants => {
     this.setState({
       restaurants: restaurants.filter((rest) => {
         return (
@@ -81,7 +70,7 @@ class Home extends React.Component {
         );
       }),
     });
-  };
+  }
 
   distance = (lat1, lng1, lat2, lng2) => {
     const R = 3958;
@@ -93,9 +82,9 @@ class Home extends React.Component {
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(rLat1) *
-        Math.cos(rLat2) *
-        Math.sin(dLng / 2) *
-        Math.sin(dLng / 2);
+      Math.cos(rLat2) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     console.log(c * R);
@@ -151,10 +140,21 @@ class Home extends React.Component {
                 .slice(0, 5)
                 .map((restaurant) => (
                   <RestaurantCard key={restaurant.id.value} restaurant={restaurant} />
-                ))} 
-          </div> */}
+                ))} */}
+          </div>
+          <div className="card-body m-2">
+            <div>
+              <Filter />
+            </div>
+            <div className="align-content-stretch flex-wrap">
+              <MapView
+                lat={this.state.lat}
+                lng={this.state.lng}
+                restaurants={this.state.restaurants}
+              />
+            </div>
+          </div>
         </div>
-      </div>
     );
   }
 }
